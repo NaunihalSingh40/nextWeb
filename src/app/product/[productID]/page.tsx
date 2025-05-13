982a9723b62df376574a0df7ebcbad1a2db7a941
Rajveerk143 <razdhanjal02@gmail.com>
2025-05-13 23:59:49 +0530

    added productpage

---
+++ src/app/product/[productID]/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+
+const page = () => {
+  return (
+    <>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <ProductPage>hahahahahahahahaha</ProductPage>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+    </>
+  );
+};
+
+export default page;
+
+const ProductPage = styled.div`
+  color: red;
+  margin-top: 20px;
+`;
